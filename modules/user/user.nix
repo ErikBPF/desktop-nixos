16b14c11bc83fea1fe36d4ad41593918d17f0cad
Erik Bogado <erikbogado@gmail.com>
2024-03-24 15:21:45 -0300

    update configs

---
+++ modules/user/user.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, ... }:
+{
+  users.users.erik = {
+    isNormalUser = true;
+    initialPassword = "test";
+    description = "Erik Bogado";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+            firefox
+            git
+            #  thunderbird
+            ];
+  };
+
+  programs.git.config = {
+    user.name = "Erik Bogado";
+    user.email = secrets.misc.email;
+  };
+}
