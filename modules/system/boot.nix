8bb546488ead2651928baf73c86be3b0dd447bd6
Erik Bogado <erikbogado@gmail.com>
2024-04-08 18:04:12 -0300

    rm

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -11,7 +11,6 @@
     kernelParams = [ "fastboot" ];
     consoleLogLevel = 3;
     initrd.verbose = false;
-    initrd.systemd.enable = true;
     initrd.postDeviceCommands = lib.mkAfter ''
       mkdir /btrfs_tmp
       mount /dev/root_vg/root /btrfs_tmp
