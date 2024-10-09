2073cb5509b1ad62053d280a972c73566aaa9b29
Erik Bogado <erikbogado@gmail.com>
2024-10-09 14:33:51 -0300

    add qwerty-fr

--- system/nix/default.nix
+++ system/nix/default.nix
@@ -14,6 +14,22 @@
   # we need git for flakes
   environment.systemPackages = [pkgs.git];
 
+  services.xserver = {
+    # ...
+
+    xkb = {
+      layout = "qwerty-fr";
+      variant = "qwerty-fr";
+      extraLayouts = {
+        qwerty-fr = {
+          description = "QWERTY with French symbols and diacritics";
+          languages = ["eng"];
+          symbolsFile = /home/erik/desktop-nixos/system/nix/us_qwerty-fr;
+        };
+      };
+    };
+  };
+
   nix = {
     package = pkgs.lix;
 
