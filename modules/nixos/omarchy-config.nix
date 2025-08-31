8da794dccf28b0f6f2232d53f48d3599bf293ba4
eribpf <erikbogado@gmail.com>
2025-08-31 01:07:22 -0300

    add hyprland settings

--- modules/nixos/omarchy-config.nix
+++ modules/nixos/omarchy-config.nix
@@ -7,6 +7,31 @@
     inputs.omarchy-nix.nixosModules.default
   ];
 
+  wayland.windowManager.hyprland.settings = {
+    # Environment variables
+    # https://wiki.hyprland.org/Configuring/Variables/#input
+    input = lib.mkDefault {
+      kb_layout = "us";
+      kb_variant = "qwerty-fr";
+      # kb_model =
+      kb_options = compose:caps;
+      # kb_rules =
+      numlock_by_default = true;
+
+      follow_mouse = 1;
+      mouse_refocus = 1;
+      float_switch_override_focus = 1;
+      scroll_factor = 0.5;
+
+      sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
+
+      touchpad = {
+        natural_scroll = false;
+      };
+
+    sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
+    };
+
   omarchy = {
     full_name = "Erik Bogado";
     email_address = "erikbogado@gmail.com";
