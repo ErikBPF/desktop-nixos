97722e5c137939a172cf711595c8c628a66d38a0
eribpf <erikbogado@gmail.com>
2025-09-05 20:30:52 -0300

    remove podman

--- modules/nixos/virtualization.nix
+++ modules/nixos/virtualization.nix
@@ -14,12 +14,12 @@
       };
     };
     docker.enable = true;
-    podman = {
-      enable = true;
+    # podman = {
+    #   enable = true;
 
-      dockerCompat = true;
-      defaultNetwork.settings.dns_enabled = true;
-    };
+    #   dockerCompat = true;
+    #   defaultNetwork.settings.dns_enabled = true;
+    # };
   };
 
   environment.systemPackages = with pkgs; [
