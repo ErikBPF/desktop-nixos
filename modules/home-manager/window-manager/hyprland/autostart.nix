28f96228e277bc7dadcdb2900063fb18e36264e2
eribpf <erikbogado@gmail.com>
2025-09-08 11:06:23 -0300

    add waybar

--- modules/home-manager/window-manager/hyprland/autostart.nix
+++ modules/home-manager/window-manager/hyprland/autostart.nix
@@ -6,7 +6,7 @@
   wayland.windowManager.hyprland.settings = {
     exec-once = [
       # "hypridle & mako & waybar & fcitx5"
-      # "waybar"
+      "waybar"
       # "swaybg -i ~/.config/omarchy/current/background -m fill"
       "hyprsunset"
       "systemctl --user start hyprpolkitagent"
