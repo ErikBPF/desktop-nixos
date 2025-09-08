4f97bdd61a8d681d728987bda0acf4315f4d4a45
eribpf <erikbogado@gmail.com>
2025-09-08 20:38:41 -0300

    update waybar

--- modules/home-manager/window-manager/waybar.nix
+++ modules/home-manager/window-manager/waybar.nix
@@ -34,72 +34,125 @@ in {
       {
         layer = "top";
         position = "top";
-        spacing = 0;
-        height = 26;
+        height = 28;
+        spacing = 0; # To match style.css
         modules-left = [
+          "custom/launch_wofi"
           "hyprland/workspaces"
+          "hyprland/window"
         ];
         modules-center = [
+          "custom/lock_screen"
+          "custom/updates"
           "clock"
         ];
         modules-right = [
-          # "custom/dropbox"
-          "tray"
-          "bluetooth"
-          "network"
-          "wireplumber"
           "cpu"
-          "power-profiles-daemon"
+          "memory"
+          "temperature"
+          "disk"
+          "backlight"
           "battery"
+          "pulseaudio"
+          "pulseaudio#microphone"
+          "tray"
+          "custom/power_btn"
         ];
+
+        "hyprland/window" = {
+          format = "{}";
+        };
+
+        "custom/launch_wofi" = {
+          format = "";
+          on-click = "pkill wofi; wofi -n";
+          tooltip = false;
+        };
+
+        "custom/lock_screen" = {
+          format = "";
+          on-click = "sh -c '(sleep 0.5s; swaylock)' & disown";
+          tooltip = false;
+        };
+
+        "custom/power_btn" = {
+          format = "";
+          on-click = "sh -c '(sleep 0.5s; wlogout --protocol layer-shell)' & disown";
+          tooltip = false;
+        };
+
+        cpu = {
+          interval = 10;
+          format = " {usage}%";
+          "max-length" = 10;
+          on-click = "ghostty --start-as=fullscreen --title btop sh -c 'btop'";
+        };
+
+        disk = {
+          interval = 30;
+          format = "󰋊 {percentage_used}%";
+          path = "/";
+          tooltip = true;
+          "tooltip-format" = "HDD - {used} used out of {total} on {path} ({percentage_used}%)";
+          on-click = "ghostty --start-as=fullscreen --title btop sh -c 'btop'";
+        };
+
+        memory = {
+          interval = 30;
+          format = " {}%";
+          "max-length" = 10;
+          tooltip = true;
+          "tooltip-format" = "Memory - {used:0.1f}GB used";
+          on-click = "ghostty --start-as=fullscreen --title btop sh -c 'btop'";
+        };
+
+        "custom/updates" = {
+          format = "{}";
+          exec = "~/.config/waybar/scripts/update-sys";
+          on-click = "~/.config/waybar/scripts/update-sys update";
+          interval = 300;
+          tooltip = true;
+        };
+
         "hyprland/workspaces" = {
+          "disable-scroll" = true;
+          "all-outputs" = true;
           on-click = "activate";
-          format = "{icon}";
-          format-icons = {
-            default = "";
-            "1" = "1";
-            "2" = "2";
-            "3" = "3";
-            "4" = "4";
-            "5" = "5";
-            "6" = "6";
-            "7" = "7";
-            "8" = "8";
-            "9" = "9";
-            active = "󱓻";
-          };
-          persistent-workspaces = {
+          "persistent_workspaces" = {
             "1" = [];
             "2" = [];
             "3" = [];
             "4" = [];
             "5" = [];
+            "6" = [];
+            "7" = [];
+            "8" = [];
+            "9" = [];
+            "10" = [];
           };
         };
-        cpu = {
-          interval = 5;
-          format = "󰍛";
-          on-click = "ghostty -e btop";
+
+        tray = {
+          "icon-size" = 18;
+          spacing = 10;
         };
+
         clock = {
-          format = "{:%A %I:%M %p}";
-          format-alt = "{:%d %B W%V %Y}";
-          tooltip = false;
+          format = "{:%d/%m/%Y - %H:%M}";
+          tooltip = true;
+          "tooltip-format" = "{: %A, %e %B %Y}";
         };
-        network = {
-          format-icons = ["󰤯" "󰤟" "󰤢" "󰤥" "󰤨"];
-          format = "{icon}";
-          format-wifi = "{icon}";
-          format-ethernet = "󰀂";
-          format-disconnected = "󰖪";
-          tooltip-format-wifi = "{essid} ({frequency} GHz)\n⇣{bandwidthDownBytes}  ⇡{bandwidthUpBytes}";
-          tooltip-format-ethernet = "⇣{bandwidthDownBytes}  ⇡{bandwidthUpBytes}";
-          tooltip-format-disconnected = "Disconnected";
-          interval = 3;
-          nospacing = 1;
-          on-click = "ghostty -e nmcli";
+
+        backlight = {
+          device = "intel_backlight";
+          format = "{icon} {percent}%";
+          "format-icons" = [ "" "" "" ];
+          "on-scroll-up" = "brightnessctl set 1%+";
+          "on-scroll-down" = "brightnessctl set 1%-";
+          "min-length" = 6;
         };
-        battery = {
+
+   battery = {
           interval = 5;
           format = "{capacity}% {icon}";
           format-discharging = "{icon}";
@@ -139,43 +192,44 @@ in {
             critical = 10;
           };
         };
-        bluetooth = {
-          format = "󰂯";
-          format-disabled = "󰂲";
-          format-connected = "";
-          tooltip-format = "Devices connected: {num_connections}";
-          on-click = "blueberry";
-        };
-        wireplumber = {
-          # Changed from "pulseaudio"
-          "format" = "";
-          format-muted = "󰝟";
-          scroll-step = 5;
-          on-click = "pavucontrol";
-          tooltip-format = "Playing at {volume}%";
-          on-click-right = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"; # Updated command
-          max-volume = 150; # Optional: allow volume over 100%
-        };
-        tray = {spacing = 13;};
-        power-profiles-daemon = {
-          format = "{icon}";
-          tooltip-format = "Power profile: {profile}";
-          tooltip = true;
-          format-icons = {
-            power-saver = "󰡳";
-            balanced = "󰊚";
-            performance = "󰡴";
+
+        pulseaudio = {
+          format = "{icon} {volume}%";
+          "format-muted" = "";
+          "on-click" = "pamixer -t";
+          "on-click-right" = "pavucontrol";
+          "on-scroll-up" = "pamixer -i 5";
+          "on-scroll-down" = "pamixer -d 5";
+          "scroll-step" = 5;
+          "format-icons" = {
+            headphone = "";
+            "hands-free" = "";
+            headset = "";
+            phone = "";
+            portable = "";
+            car = "";
+            default = [ "" "" "" ];
           };
         };
-        # "custom/dropbox" = {
-        #   format = "";
-        #   on-click = "nautilus ~/Dropbox";
-        #   exec = "dropbox-cli status";
-        #   return-type = "text";
-        #   interval = 5;
-        #   tooltip = true;
-        #   tooltip-format = "{}";
-        # };
+
+        "pulseaudio#microphone" = {
+          format = "{format_source}";
+          "format-source" = " {volume}%";
+          "format-source-muted" = "";
+          "on-click" = "pamixer --default-source -t";
+          "on-click-right" = "pavucontrol";
+          "on-scroll-up" = "pamixer --default-source -i 5";
+          "on-scroll-down" = "pamixer --default-source -d 5";
+          "scroll-step" = 5;
+        };
+
+        temperature = {
+          "thermal-zone" = 1;
+          format = " {temperatureC}°C";
+          "critical-threshold" = 70;
+          "format-critical" = " {temperatureF}°F";
+          on-click = "ghostty --start-as=fullscreen --title btop sh -c 'btop'";
+        };
       }
     ];
   };
