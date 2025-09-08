a5507f56c793e97661c9a25a6726f1441da5e0fa
eribpf <erikbogado@gmail.com>
2025-09-08 18:05:14 -0300

    add few pkgs

--- modules/packages.nix
+++ modules/packages.nix
@@ -30,6 +30,7 @@ let
     eza
     fd
     curl
+    direnv
     unzip
     wget
     gnumake
@@ -37,6 +38,13 @@ let
 
   # Discretionary packages - can be excluded by user
   discretionaryPackages = with pkgs; [
+
+    swappy
+    wf-recorder
+    grim
+    slurp
+
+
     # TUIs
     lazygit
     lazydocker
@@ -48,11 +56,31 @@ let
     chromium
     vlc
 
+    # Coding
+    python3Full
+    uv
+    go
+    go-protobuf
+    protoc-gen-go
+    protoc-gen-go-grpc
+    zulu23
+    jre21_minimal
+    zulu17
+    zulu11
+
+    spark
+
+    terraform
+    terragrunt
+    tenv
     # Development tools
     github-desktop
     gh
 
     code-cursor
+    opencode
+    gemini-cli
+    ngrok
     # Containers
     docker-compose
     ffmpeg
@@ -63,8 +91,20 @@ let
     lens
     dbeaver-bin
     azure-cli
+    azure-storage-azcopy
+    discord
+
+        foot
+    kitty
+    
+    # Terminal utilities
+    starship  # customizable prompt
+    
+    # Wayland terminal tools
+    ydotool   # input automation for Wayland
+    wtype     # text input for Wayland
+    wl-clipboard  # clipboard utilities for Wayland
   ] ++ lib.optionals (pkgs.system == "x86_64-linux") [
-    typora
     spotify
   ];
 
@@ -80,5 +120,59 @@ in {
     neofetch
     fastfetch
     nordpass
+    tailscale
+    ente-auth
+
+
+    btop
+    iotop  # io monitoring
+    iftop  # network monitoring
+    
+    # System monitoring and debugging
+    strace  # system call monitoring
+    ltrace  # library call monitoring
+    lsof    # list open files
+    sysstat
+    lm_sensors  # for `sensors` command
+    ethtool
+    pciutils  # lspci
+    usbutils  # lsusb
+    
+    # Calculator
+    wofi-calc
+
+        # Archive utilities
+    zip
+    xz
+    unzip
+    p7zip
+    
+    # Command line utilities
+    ripgrep  # recursively searches directories for a regex pattern
+    jq       # A lightweight and flexible command-line JSON processor
+    yq-go    # yaml processer
+    eza      # A modern replacement for 'ls'
+    fzf      # A command-line fuzzy finder
+    
+    # Networking tools
+    mtr      # A network diagnostic tool
+    iperf3
+    dnsutils # `dig` + `nslookup`
+    ldns     # replacement of `dig`, provides `drill`
+    aria2    # multi-protocol download utility
+    socat    # replacement of openbsd-netcat
+    nmap     # network discovery and security auditing
+    ipcalc   # IPv4/v6 address calculator
+    
+    # Misc utilities
+    cowsay
+    file
+    which
+    tree
+    gnused
+    gnutar
+    gawk
+    zstd
+    gnupg
   ];
 }
