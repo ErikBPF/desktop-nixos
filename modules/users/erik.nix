c60cc59439f3f3ed1cb8c51026982e9d6a7e856d
eribpf <erikbogado@gmail.com>
2025-09-09 19:38:47 -0300

    rm comment

--- modules/users/erik.nix
+++ modules/users/erik.nix
@@ -5,7 +5,6 @@
       initialPassword = "1045";
       openssh.authorizedKeys.keys =
   [
-    # change this to your ssh key
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMxdE+uAvR4Nm2XwZNjTf2Ae8PlrRtnZUI6BBrbGl78u erikbogado@gmail.com"
   ];
 
