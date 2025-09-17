ce0f79ef2088f069caa05186563609b8aee450d4
eribpf <erikbogado@gmail.com>
2025-09-17 01:43:09 -0300

    update kitty

--- modules/home-manager/terminal/kitty.nix
+++ modules/home-manager/terminal/kitty.nix
@@ -1,6 +1,14 @@
 {...}: {
   programs.kitty = {
     enable = true;
-    # enableFishIntegration = true;
+    settings = {
+      font_size    =    12;
+      cursor_shape =    "block";
+      url_color       = "#0087bd";
+      url_style      =" dotted";
+      #Close the terminal without confirmation
+      confirm_os_window_close =0;
+      background_opacity =0.97;
+    };
   };
 }
