30de20ac2dc6795c7f97605f6437d984d0031815
eribpf <erikbogado@gmail.com>
2025-09-17 01:00:57 -0300

    update font

--- modules/home-manager/terminal/ghostty.nix
+++ modules/home-manager/terminal/ghostty.nix
@@ -2,5 +2,9 @@
   programs.ghostty = {
     enable = true;
     enableFishIntegration = true;
+    settings = {
+      font-family = "JetBrainsMono Nerd Font";
+      font-size = 12;
+    };
   };
 }
