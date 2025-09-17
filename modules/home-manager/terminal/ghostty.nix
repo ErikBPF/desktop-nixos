ab1d08ce7cd9f665dcd780279a98ba18ba541fd0
eribpf <erikbogado@gmail.com>
2025-09-17 01:27:45 -0300

    cg

--- modules/home-manager/terminal/ghostty.nix
+++ modules/home-manager/terminal/ghostty.nix
@@ -5,7 +5,7 @@
     settings = {
       font-family = "JetBrainsMono Nerd Font";
       font-size = 10;
-      freetype-load-flags = no-force-autohint;
+      freetype-load-flags = "no-force-autohint";
       # adjust-cell-width = "-10%";
     };
   };
