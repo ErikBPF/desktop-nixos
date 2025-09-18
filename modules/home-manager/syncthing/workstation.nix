d0384e4e2ed4eeae36d8bc1fa6e9ca8aa7686841
eribpf <erikbogado@gmail.com>
2025-09-17 22:37:00 -0300

    rm syncthing

--- modules/home-manager/syncthing/workstation.nix
+++ modules/home-manager/syncthing/workstation.nix
@@ -15,30 +15,30 @@
     ${builtins.readFile config.sops.secrets."syncthing/moon_id".path}
     '';
 
-  services.syncthing = {
-    overrideDevices = true;
-    overrideFolders = true;
-    tray = true;
-    settings = {
-      devices = {
-        "Moon" = {
-          id = builtins.readFile config.sops.secrets."syncthing/moon_id".path;
-        };
-        "archlinux" = {
-          id = builtins.readFile config.sops.secrets."syncthing/archlinux_id".path;
-        };
-      };
+  # services.syncthing = {
+  #   overrideDevices = true;
+  #   overrideFolders = true;
+  #   tray = true;
+  #   settings = {
+  #     devices = {
+  #       "Moon" = {
+  #         id = builtins.readFile config.sops.secrets."syncthing/moon_id".path;
+  #       };
+  #       "archlinux" = {
+  #         id = builtins.readFile config.sops.secrets."syncthing/archlinux_id".path;
+  #       };
+  #     };
 
-      folders = {
-        "ndykv-cjhly" = {
-          label = "Downloads";
-          path = "/home/erik/Downloads/";
-          devices = [
-            "Moon"
-            "archlinux"
-          ];
-        };
-      };
-    };
-  };
+  #     folders = {
+  #       "ndykv-cjhly" = {
+  #         label = "Downloads";
+  #         path = "/home/erik/Downloads/";
+  #         devices = [
+  #           "Moon"
+  #           "archlinux"
+  #         ];
+  #       };
+  #     };
+  #   };
+  # };
 }
