2cfb9f12f7be5508cea62e8dbfbc957374aa4137
eribpf <erikbogado@gmail.com>
2025-09-18 14:50:27 -0300

    remove var

--- modules/home-manager/window-manager/hyprland/envs.nix
+++ modules/home-manager/window-manager/hyprland/envs.nix
@@ -49,8 +49,6 @@ in {
 
         # GTK theme
         "GTK_THEME,Adwaita:dark"
-
-        "DIRENV_LOG_FORMAT,"
         # Podman compatibility. Probably need to add cfg.env?
         # "DOCKER_HOST,unix://$XDG_RUNTIME_DIR/podman/podman.sock"
       ];
