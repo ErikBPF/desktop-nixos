b20a4b340fdaab20063b043f531dd9c7dfae2d59
eribpf <erikbogado@gmail.com>
2025-09-17 21:59:15 -0300

    add syncthing

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -89,7 +89,7 @@ in {
 
   services.syncthing = {
       enable = true;
-      guiAddress = "0.0.0.0:8384";
+      guiAddress = "127.0.0.1:8384";
       openDefaultPorts = true;
       relay = {
         enable = true;
