a33a8414fbc848abd8c39fb705b1e5353cfb4155
eribpf <erikbogado@gmail.com>
2025-09-17 21:59:15 -0300

    add syncthing

--- hosts/workstation/default.nix
+++ hosts/workstation/default.nix
@@ -39,6 +39,40 @@
 
   services.openssh.enable = true;
 
+  sops = {
+    secrets = {
+      "syncthing/moon_id"  = {};
+      "syncthing/archlinux_id" = {};
+    };
+  };
+
+  services.syncthing = {
+    overrideDevices = true;
+    overrideFolders = true;
+    configDir = "/home/erik/.config/syncthing";
+    settings = {
+      devices = {
+        "Moon" = {
+          id = builtins.readFile config.sops.secrets."syncthing/moon_id".path;
+        };
+        "archlinux" = {
+          id = builtins.readFile config.sops.secrets."syncthing/archlinux_id".path;
+        };
+      };
+
+      folders = {
+        "ndykv-cjhly" = {
+          label = "Downloads";
+          path = "/home/erik/Downloads/";
+          devices = [
+            "Moon"
+            "archlinux"
+          ];
+        };
+      };
+    };
+  };
+
   home-manager.useGlobalPkgs = true;
   home-manager.backupFileExtension = "backup";
   home-manager.extraSpecialArgs = {inherit inputs outputs;};
