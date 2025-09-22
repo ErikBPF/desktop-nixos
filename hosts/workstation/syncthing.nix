aa165285a10602ed80a33e12cd1690724e1a411c
eribpf <erikbogado@gmail.com>
2025-09-22 10:28:47 -0300

    tmp remove syncthing

--- hosts/workstation/syncthing.nix
+++ hosts/workstation/syncthing.nix
@@ -16,54 +16,54 @@
       "syncthing/archlinux_id" = {};
     };
   };
-  services.syncthing = {
-    enable = true;
-    guiAddress = "127.0.0.1:8384";
-    openDefaultPorts = true;
-    relay = {
-      enable = true;
-    };
-    configDir = "/home/erik/.config/syncthing";
-    dataDir = "/home/erik/.config/syncthing";
-    overrideDevices = true;
-    overrideFolders = true;
-    user = "erik";
-    settings = {
-      devices = {
-        "Moon" = {
-          id = ''${builtins.readFile config.sops.secrets."syncthing/moon_id".path}'';
-        };
-        "archlinux" = {
-          id = ''${builtins.readFile config.sops.secrets."syncthing/archlinux_id".path}'';
-        };
-      };
+  # services.syncthing = {
+  #   enable = true;
+  #   guiAddress = "127.0.0.1:8384";
+  #   openDefaultPorts = true;
+  #   relay = {
+  #     enable = true;
+  #   };
+  #   configDir = "/home/erik/.config/syncthing";
+  #   dataDir = "/home/erik/.config/syncthing";
+  #   overrideDevices = true;
+  #   overrideFolders = true;
+  #   user = "erik";
+  #   settings = {
+  #     devices = {
+  #       "Moon" = {
+  #         id = ''${builtins.readFile config.sops.secrets."syncthing/moon_id".path}'';
+  #       };
+  #       "archlinux" = {
+  #         id = ''${builtins.readFile config.sops.secrets."syncthing/archlinux_id".path}'';
+  #       };
+  #     };
 
-      folders = {
-        "ndykv-cjhly" = {
-          label = "Downloads";
-          path = "/home/erik/Downloads/";
-          devices = [
-            "Moon"
-            "archlinux"
-          ];
-        };
-        "ykxhp-khmz2" = {
-          label = "Documents";
-          path = "/home/erik/Documents/";
-          devices = [
-            "Moon"
-            "archlinux"
-          ];
-        };
-        "xbwsp-zwvsr" = {
-          label = "kube";
-          path = "/home/erik/.kube/";
-          devices = [
-            "Moon"
-            "archlinux"
-          ];
-        };
-      };
-    };
-  };
+  #     folders = {
+  #       "ndykv-cjhly" = {
+  #         label = "Downloads";
+  #         path = "/home/erik/Downloads/";
+  #         devices = [
+  #           "Moon"
+  #           "archlinux"
+  #         ];
+  #       };
+  #       "ykxhp-khmz2" = {
+  #         label = "Documents";
+  #         path = "/home/erik/Documents/";
+  #         devices = [
+  #           "Moon"
+  #           "archlinux"
+  #         ];
+  #       };
+  #       "xbwsp-zwvsr" = {
+  #         label = "kube";
+  #         path = "/home/erik/.kube/";
+  #         devices = [
+  #           "Moon"
+  #           "archlinux"
+  #         ];
+  #       };
+  #     };
+  #   };
+  # };
 }
