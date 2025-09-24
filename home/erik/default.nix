a0b4acbbcf95ee0b40e8de2b0dd7a10ebd605dae
eribpf <erikbogado@gmail.com>
2025-09-24 16:43:01 -0300

    move gpg

--- home/erik/default.nix
+++ home/erik/default.nix
@@ -45,6 +45,13 @@
     enable = true;
     enableFishIntegration = true;
   };
+  services.gpg-agent = {
+      enable = true;
+      enableSshSupport = true;
+      enableExtraSocket = true;
+      sshKeys = ["D528D50F4E9F031AACB1F7A9833E49C848D6C90"];
+      pinentryPackage = pkgs.pinentry-gnome3;
+    };
 
   sops = {
     age = {
