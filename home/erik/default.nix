f1546c5fe11a5f7ce6e198ce5bbbdd13f97e1401
eribpf <erikbogado@gmail.com>
2025-09-24 16:53:25 -0300

    up

--- home/erik/default.nix
+++ home/erik/default.nix
@@ -50,7 +50,7 @@
       enableSshSupport = true;
       enableExtraSocket = true;
       sshKeys = ["D528D50F4E9F031AACB1F7A9833E49C848D6C90"];
-      pinentryPackage = pkgs.pinentry-gnome3;
+      pinentry.package = pkgs.pinentry-gnome3;
     };
 
   sops = {
