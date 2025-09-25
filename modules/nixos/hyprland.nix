a783bece62bcd42f074c241bf62c77b3567a72dd
eribpf <erikbogado@gmail.com>
2025-09-25 18:44:05 -0300

    enable hyprland

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -4,7 +4,7 @@ inputs: {
   ...
 }: {
   programs.hyprland = {
-    enable = false;
+    enable = true;
     # package = inputs.hyprland.packages.${pkgs.system}.hyprland;
     # package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     # portalPackage = pkgs.xdg-desktop-portal-hyprland; # Use stable nixpkgs version to fix Qt version mismatch
