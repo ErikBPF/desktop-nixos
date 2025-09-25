415feeb53929936436228db0ec99dbd0a3ff4fad
eribpf <erikbogado@gmail.com>
2025-09-25 09:39:30 -0300

    change hypr

--- modules/home-manager/window-manager/hyprland/configuration.nix
+++ modules/home-manager/window-manager/hyprland/configuration.nix
@@ -12,7 +12,8 @@
     ./looknfeel.nix
     ./windows.nix
   ];
-  wayland.windowManager.hyprland.settings = {
+  wayland.windowManager.hyprland = {
+    settings = {
     # Default applications
     "$terminal" = lib.mkDefault "ghostty";
     "$fileManager" = lib.mkDefault "nautilus --new-window";
@@ -20,19 +21,20 @@
     "$music" = lib.mkDefault "spotify";
     "$webapp" = lib.mkDefault "$browser --app";
 
-    monitor = [];
-    extraConfig = ''
-
+    monitor = [
       # Easily plug in any monitor
-      monitor=,preferred,auto,1
+      ",preferred,auto,1"
 
       # 1080p-HDR monitor on the left, 4K-HDR monitor in the middle and 1080p vertical monitor on the right.
-      monitor=eDP-1,preferred,1592x1680,1.25
+      "eDP-1,preferred,1592x1680,1.25"
       # monitor=HDMI-1,preferred,auto,1,mirror,eDP-1
-      monitor=desc:Samsung Electric Company QBQ90 0x01000E00,2560x1440,1080x240,1#,bitdepth,10
-      monitor=desc:Samsung Electric Company C27F390 HX5MB00876,1920x1080,0x0,1,transform,1
-      monitor=desc:Samsung Electric Company C27F390 HX5MB00881,1920x1080,3640x0,1,transform,3
-
+      "desc:Samsung Electric Company QBQ90 0x01000E00,2560x1440,1080x240,1#,bitdepth,10"
+      "desc:Samsung Electric Company C27F390 HX5MB00876,1920x1080,0x0,1,transform,1"
+      "desc:Samsung Electric Company C27F390 HX5MB00881,1920x1080,3640x0,1,transform,3"
+    ];
+   
+    };
+    extraConfig = ''
       # Binds workspaces to my monitors only (find desc with: hyprctl monitors)
       workspace = 1, monitor:desc:Samsung Electric Company QBQ90 0x01000E00, default:true
       workspace = 2, monitor:desc:Samsung Electric Company QBQ90 0x01000E00
