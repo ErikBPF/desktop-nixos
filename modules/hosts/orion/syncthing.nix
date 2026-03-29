e006e7d7007806bbba06f60388c22c7622411c3e
eribpf <erikbogado@gmail.com>
2026-03-29 18:45:31 -0300

    feat: Created orion-containers Podman module with dockerCompat and home…
    - "modules/hosts/orion/containers.nix"
    - "modules/hosts/orion/syncthing.nix"
    - "modules/hosts/orion/default.nix"
    - "modules/secrets.nix"
    
    GSD-Task: S04/T01

--- modules/hosts/orion/syncthing.nix
+++ modules/hosts/orion/syncthing.nix
@@ -5,7 +5,7 @@ in {
     homeDir = "/home/erik";
   in {
     services.syncthing = {
-      enable = false;
+      enable = true;
       guiAddress = "127.0.0.1:8384";
       openDefaultPorts = false;
       relay.enable = false;
@@ -16,7 +16,11 @@ in {
       user = "erik";
       settings = {
         options.listenAddresses = ["tcp://0.0.0.0:22000" "tcp://[::]:22000"];
-        devices = {};
+        devices = {
+          "discovery".id = deviceIDs.discovery_id;
+          "kepler".id = deviceIDs.kepler_id;
+        };
+        # Future: add /opt/models folder sync from Kepler after first boot
         folders = {};
       };
     };
