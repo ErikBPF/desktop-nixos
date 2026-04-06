15234f70407d0c7d2d05374d644da712109241d7
eribpf <erikbogado@gmail.com>
2026-04-06 18:20:33 -0300

    add new claude-code

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -3,6 +3,7 @@
     nixpkgs.overlays = [
       (final: _prev: {
         quickshell = inputs.quickshell.packages.${final.stdenv.hostPlatform.system}.default;
+        claude-code = inputs.claude-code-nix.packages.${final.stdenv.hostPlatform.system}.default;
       })
     ];
   };
