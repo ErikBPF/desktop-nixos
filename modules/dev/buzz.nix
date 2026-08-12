56034cd7f5644e41f00b1869e89e9dbef6ee5a0c
Erik Bogado <erikbogado@gmail.com>
2026-08-12 16:37:48 -0300

    fix(buzz): use Pangolin relay URL

--- modules/dev/buzz.nix
+++ modules/dev/buzz.nix
@@ -2,6 +2,6 @@
   flake.modules.home.buzz = {
     imports = [inputs.buzz-flake.homeManagerModules.withPackage];
     programs.buzz.enable = true;
-    home.sessionVariables.BUZZ_RELAY_URL = "http://kepler:3000";
+    home.sessionVariables.BUZZ_RELAY_URL = "wss://buzz.homelab.pastelariadev.com";
   };
 }
