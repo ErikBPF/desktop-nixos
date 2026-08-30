3a9a79e9447e69832219e7f6f3f08aed3b03dfee
Erik Bogado <erikbogado@gmail.com>
2026-08-30 00:50:14 -0300

    fix(discovery): include managed Cognee repo (#250)

--- flake.nix
+++ flake.nix
@@ -124,7 +124,7 @@
 
     # Runtime jobs execute this reviewed source revision, never a mutable checkout.
     homelab-iac = {
-      url = "github:ErikBPF/homelab-iac/854745c63ce6df69807da53ccdb57a465a3845d1";
+      url = "github:ErikBPF/homelab-iac/dc54f16afcf4f5b797c676a043903c5729786181";
       flake = false;
     };
 
